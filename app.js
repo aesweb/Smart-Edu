f4e2051a7e871f01ed83fd096038cba9f68a67a9
Ahmet Eren Sancar <aes.sncr@gmail.com>
2021-06-05 13:38:27 +0300

    template engine created

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require("express");
 
-//MİDDLEWARES
 const app = express();
 
+//TEMPLATE ENGİNES
+
+app.set("view engine", "ejs");
+
+//MİDDLEWARES
+app.use(express.static("public"));
+
+//ROUTES
 app.get("/", (req, res) => {
-  res.send("index page");
+  res.status(200).render("index", {
+    page_name: "index"
+  });
 });
+
+app.get("/about", (req, res) => {
+    res.status(200).render("about", {
+      page_name: "about"
+    });
+  });
+  
 
 const port = 3000;
 app.listen(port, () => {
