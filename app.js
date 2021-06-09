b8fdfca81eb0b8a658f7ebccef10d26916030088
Ahmet Eren Sancar <aes.sncr@gmail.com>
2021-06-09 19:50:35 +0300

    login + register

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const pageRoute = require("./routes/pageRoute");
+const courseRoute = require("./routes/courseRoute");
+const categoryRoute = require("./routes/categoryRoute");
+const userRoute = require("./routes/userRoute");
 
 const app = express();
+
+//CONNECT DB
+mongoose
+  .connect("mongodb://localhost/smartedu-db", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB CONNECTED SUCCESFULY");
+  });
 
 //TEMPLATE ENGİNES
 
@@ -8,20 +25,14 @@ app.set("view engine", "ejs");
 
 //MİDDLEWARES
 app.use(express.static("public"));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 //ROUTES
-app.get("/", (req, res) => {
-  res.status(200).render("index", {
-    page_name: "index"
-  });
-});
-
-app.get("/about", (req, res) => {
-    res.status(200).render("about", {
-      page_name: "about"
-    });
-  });
-  
+app.use("/", pageRoute);
+app.use("/courses", courseRoute);
+app.use("/categories", categoryRoute);
+app.use("/users", userRoute);
 
 const port = 3000;
 app.listen(port, () => {
