bbbf864827a419452a1c599c027bf5c18a231ce1
Ahmet Eren Sancar <aes.sncr@gmail.com>
2021-06-14 16:59:53 +0300

    Course Teacher Relationship

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,8 @@
-const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const Category = require("../models/Category");
+const User = require("../models/User");
+const Course = require("../models/Course");
+
 
 exports.createUser = async (req, res) => {
   try {
@@ -43,9 +46,13 @@ exports.logoutUser = (req, res) => {
 };
 
 exports.getDashboardPage = async (req, res) => {
-  const user = await User.findOne({ _id: req.session.userID });
+  const user = await User.findOne({ _id: req.session.userID })
+  const categories = await Category.find();
+  const courses = await Course.find({user:req.session.userID})
   res.status(200).render("dashboard", {
     page_name: "dashboard",
     user,
+    categories,
+    courses
   });
 };
