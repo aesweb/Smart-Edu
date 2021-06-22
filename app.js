e813578c0ec23f0003a429b61a37084d383b50a7
Ahmet Eren Sancar <aes.sncr@gmail.com>
2021-06-23 00:21:11 +0300

    deficiencies were fixed, delete course button is added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
+const methodOverride = require("method-override");
 const pageRoute = require("./routes/pageRoute");
 const courseRoute = require("./routes/courseRoute");
 const categoryRoute = require("./routes/categoryRoute");
@@ -45,7 +46,13 @@ app.use(flash());
 app.use((req, res, next) => {
   res.locals.flashMessages = req.flash();
   next();
-})
+});
+
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 //ROUTES
 app.use("*", (req, res, next) => {
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,24 @@
 const bcrypt = require("bcrypt");
+const { validationResult } = require("express-validator");
 const Category = require("../models/Category");
 const User = require("../models/User");
 const Course = require("../models/Course");
 
-
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
+
     res.status(201).redirect('/login');
   } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      error,
-    });
+    const errors = validationResult(req);
+    console.log(errors);
+    console.log(errors.array()[0].msg);
+
+    for (let i = 0; i <errors.array().length; i++) {
+      req.flash("error", `${errors.array()[i].msg}`);
+    }
+  
+    res.status(400).redirect('/register');
   }
 };
 
@@ -23,17 +29,29 @@ exports.loginUser = async (req, res) => {
     await User.findOne({ email }, (err, user) => {
       if (user) {
         bcrypt.compare(password, user.password, (err, same) => {
-            // USER SESSION
+          if (same) {
             req.session.userID = user._id;
             res.status(200).redirect("/users/dashboard");
+          } else {
+            req.flash("error", "Your password is not correct!");
+            res.status(400).redirect("/login");
+          }
         });
+      } else {
+        req.flash("error", "User is not exist!");
+        res.status(400).redirect("/login");
       }
     });
   } catch (error) {
-    res.status(400).json({
-      status: "fail",
-      error,
-    });
+    const errors = validationResult(req);
+    console.log(errors);
+    console.log(errors.array()[0].msg);
+
+    for (let i = 0; i < errors.array().length; i++) {
+      req.flash("error", `${errors.array()[i].msg}`);
+    }
+
+    res.status(400).redirect("/register");
   }
 };
 
@@ -44,13 +62,15 @@ exports.logoutUser = (req, res) => {
 };
 
 exports.getDashboardPage = async (req, res) => {
-  const user = await User.findOne({ _id: req.session.userID }).populate('courses');
+  const user = await User.findOne({ _id: req.session.userID }).populate(
+    "courses"
+  );
   const categories = await Category.find();
-  const courses = await Course.find({user:req.session.userID})
+  const courses = await Course.find({ user: req.session.userID });
   res.status(200).render("dashboard", {
     page_name: "dashboard",
     user,
     categories,
-    courses
+    courses,
   });
 };
