79a2305cc115db645e19d727dd7d576569633d44
Ahmet Eren Sancar <aes.sncr@gmail.com>
2021-06-24 21:11:29 +0300

    Admin area and deleting user.

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,17 +8,17 @@ exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
 
-    res.status(201).redirect('/login');
+    res.status(201).redirect("/login");
   } catch (error) {
     const errors = validationResult(req);
     console.log(errors);
     console.log(errors.array()[0].msg);
 
-    for (let i = 0; i <errors.array().length; i++) {
+    for (let i = 0; i < errors.array().length; i++) {
       req.flash("error", `${errors.array()[i].msg}`);
     }
-  
-    res.status(400).redirect('/register');
+
+    res.status(400).redirect("/register");
   }
 };
 
@@ -67,10 +67,26 @@ exports.getDashboardPage = async (req, res) => {
   );
   const categories = await Category.find();
   const courses = await Course.find({ user: req.session.userID });
+  const users = await User.find();
   res.status(200).render("dashboard", {
     page_name: "dashboard",
     user,
     categories,
     courses,
+    users,
   });
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    await User.findOneAndRemove(req.params.id);
+    await Course.deleteMany({ user: req.params.id });
+
+    res.status(200).redirect("/users/dashboard");
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
 };
