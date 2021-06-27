a5e524ea3cfc892e32754fe1bba87a84d21a68dd
Ahmet Eren Sancar <aes.sncr@gmail.com>
2021-06-27 22:32:35 +0300

    add and delete categories

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,8 +1,19 @@
 const nodemailer = require("nodemailer");
+const Course = require("../models/Course");
+const User = require("../models/User");
 
-exports.getIndexPage = (req, res) => {
+exports.getIndexPage = async (req, res) => {
+  const courses = await Course.find().sort("-createdAt").limit(2);
+  const totalCourses = await Course.find().countDocuments();
+  const totalStudents = await User.countDocuments({ role: "student" });
+  const totalTeachers = await User.countDocuments({ role: "teacher" });
+
   res.status(200).render("index", {
     page_name: "index",
+    courses,
+    totalCourses,
+    totalStudents,
+    totalTeachers,
   });
 };
 
@@ -31,10 +42,8 @@ exports.getContactPage = (req, res) => {
 };
 
 exports.sendEmail = async (req, res) => {
-
   try {
-
-  const outputMessage = `
+    const outputMessage = `
   
   <h1>Mail Details </h1>
   <ul>
@@ -43,41 +52,38 @@ exports.sendEmail = async (req, res) => {
   </ul>
   <h1>Message</h1>
   <p>${req.body.message}</p>
-  `
+  `;
 
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // true for 465, false for other ports
-    auth: {
-      user: "aes.sncr@gmail.com", // gmail account
-      pass: "fgczhslepscswoas", // gmail password
-    },
-  });
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: "aes.sncr@gmail.com", // gmail account
+        pass: "fgczhslepscswoas", // gmail password
+      },
+    });
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"Smart EDU Contact Form" <aes.sncr@gmail.com>', // sender address
-    to: "beyondx71@gmail.com", // list of receivers
-    subject: "Smart EDU Contact Form New Message ✔", // Subject line
-    html: outputMessage, // html body
-  });
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Smart EDU Contact Form" <aes.sncr@gmail.com>', // sender address
+      to: "beyondx71@gmail.com", // list of receivers
+      subject: "Smart EDU Contact Form New Message ✔", // Subject line
+      html: outputMessage, // html body
+    });
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 
+    req.flash("success", "We Received your message successfully");
 
-  req.flash("success", "We Received your message successfully");
-
-  res.status(200).redirect('contact');
-  
-} catch (err) {
-  req.flash("error", "Something happened!");
-  res.status(200).redirect('contact');
- }
+    res.status(200).redirect("contact");
+  } catch (err) {
+    req.flash("error", "Something happened!");
+    res.status(200).redirect("contact");
+  }
 };
-
